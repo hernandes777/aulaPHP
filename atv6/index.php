33856c1c64c12d3d0697baa093d0d2a19fd95bf9
hernandes777 <hernandescoms777@gmail.com>
2024-11-04 14:33:09 -0300

    Desafio Final

---
+++ atv6/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade 6</title>
+</head>
+<body>
+    <?php
+            $DIA=1
+            switch ($DIA) {
+                case 1:
+                    echo "DOMINGO"
+                    break;
+                
+                default:
+                    echo "Escolha um Número de 1 a 7"
+                    break;
+            }
+
+            
+            $DIA=2 
+            switch ($DIA) {
+                case 2:
+                    echo "SEGUNDA"
+                    break;
+                
+                default:
+                    echo "Escolha um Número de 1 a 7"
+                    break;
+            }
+
+            
+            $DIA=3
+            switch ($DIA) {
+                case 3:
+                    echo "TERÇA-FEIRA"
+                    break;
+                
+                default:
+                    echo "Escolha um Número de 1 a 7"
+                    break;
+            }
+
+            
+            $DIA=4
+            switch ($DIA) {
+                case 4:
+                    echo "QUARTA-FEIRA"
+                    break;
+                
+                default:
+                    echo "Escolha um Número de 1 a 7"
+                    break;
+            }
+            
+            $DIA=5
+            switch ($DIA) {
+                case 5:
+                    echo "QUINTA-FEIRA"
+                    break;
+                
+                default:
+                    echo "Escolha um Número de 1 a 7"
+                    break;
+            }
+            
+            $DIA=6
+            switch ($DIA) {
+                case 6:
+                    echo "SEXTA-FEIRA"
+                    break;
+                
+                default:
+                    echo "Escolha um Número de 1 a 7"
+                    break;
+            }
+            
+            $DIA=7
+            switch ($DIA) {
+                case 7:
+                    echo "SÁBADO"
+                    break;
+                
+                default:
+                    echo "Escolha um Número de 1 a 7"
+                    break;
+            }
+</body>
+</html>
---
+++ atv7/index.php
@@ -0,0 +1,23 @@
+<?php
+   $dia=1;
+   switch ($dia) {
+    case 1:
+        echo "Você escolheu: Refrigerante <br>";
+        echo "Preço: R$ 4,00";
+    break;
+
+    case 2:
+        echo "Você escolheu: Água <br>";
+         echo "Preço: R$ 2,50";
+    break;
+
+    case 3:
+        echo "Você escolheu: Suco <br>";
+         echo "Preço: R$ 3,00"
+    break;
+
+    default:
+        echo "escolha um numero de 1 a 3"
+   }
+
+  ?>
